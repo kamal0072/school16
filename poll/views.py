7e64910b05a1946b8c1f77414efdc590144ca284
Haaris <haaris@trainingbasket.in>
2024-06-05 13:31:34 +0530

    hi

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+from django.core.paginator import Paginator
 
-# Create your views here.
+def postdetail(request):
+    data = Post.objects.all().order_by('id')
+    paginator = Paginator(data, 3, orphans=2)
+    print("Paginator : ", paginator)
+    # print("Paginator : ", paginator.count)
+    # print("Paginator : ", paginator.per_page)
+    # print("Paginator : ", paginator.get_page(4))
+    page_no=request.GET.get('page')
+    print("Page Number : ", page_no)
+    print("Page Number : ", page_no)
+    page_obj = paginator.get_page(page_no)
+    print('Page Object : ', page_obj)
+    return render(request, 'detail.html', {'page_obj' : page_obj})
